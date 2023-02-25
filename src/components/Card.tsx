d9328bff285f9f6c13b6ad67b2555107107f05ce
notmanish <notageek-guy>
2023-02-25 21:48:08 +0530

    fixed card styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,28 @@
-
 const Card = ({ children }: { children: React.ReactNode }) => {
-  return <div className={`card`}>{children}</div>;
+  return (
+    <div
+      className={`
+    bg-white
+    shadow-md
+    rounded-md
+    p-4
+    sm:p-6
+    md:p-8
+    lg:p-10
+    dark:bg-gray-800
+    max-w-xl
+    mx-auto
+    w-[90%]
+    
+    sm:px-4
+    sm:py-6
+    
+  
+  `}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Card;
