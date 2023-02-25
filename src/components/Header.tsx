53c2faf1f25f0a41e6436314fb8a5b29b2957970
notmanish <notageek-guy>
2023-02-25 22:11:33 +0530

    added github link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 import { FaGithub } from "react-icons/fa";
@@ -6,10 +7,12 @@ export default function Header() {
     <div className="max-w-6xl px-2 py-4 mx-auto sm:px-4 sm:py-6">
       <div className="flex flex-col items-center gap-4">
         <h1 className="heading">Git Genie</h1>
-        <button className="px-4 py-2 text-white bg-gray-800 rounded-md">
-          <FaGithub className="inline-block mr-2" />
-          Star on Github
-        </button>
+        <Link passHref href="https://github.com/notageek-guy/git-genie">
+          <button className="px-4 py-2 text-white bg-gray-800 rounded-md">
+            <FaGithub className="inline-block mr-2" />
+            Star on Github
+          </button>
+        </Link>
         <div className="flex flex-col w-full max-w-full gap-2 mx-auto sm:max-w-4xl">
           <h1 className="text-4xl font-bold text-center text-white">
             Discover Github users and their repositories
